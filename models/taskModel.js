9b891fb92337b9da99511d43e56fef1540485ea8
Gaurav Manchanda <gaurav.manchanda@nokia.com>
2025-03-08 15:23:41 -0500

    Added update and delete functions in TaskModel

--- models/taskModel.js
+++ models/taskModel.js
@@ -9,6 +9,14 @@ class TaskModel {
         db.run("INSERT INTO tasks (title, description, due_date, priority, status) VALUES (?, ?, ?, ?, 'Pending')",
             [title, description, due_date, priority], callback);
     }
+
+    static updateTaskStatus(id, status, callback) {
+        db.run("UPDATE tasks SET status = ? WHERE id = ?", [status, id], callback);
+    }
+    
+    static deleteTask(id, callback) {
+        db.run("DELETE FROM tasks WHERE id = ?", [id], callback);
+    }    
 }
 
 module.exports = TaskModel;
