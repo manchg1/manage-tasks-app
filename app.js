dd66fcbc2245af2d64f1f50d7cd8d6bcfb9535a0
Gaurav Manchanda <gaurav.manchanda@nokia.com>
2025-03-08 16:10:23 -0500

    FIX: ReferenceError: taskController is not defined

--- app.js
+++ app.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const mustacheExpress = require('mustache-express');
 const bodyParser = require('body-parser');
+const taskController = require('./controllers/taskController');
 const app = express();
 
 app.engine('mustache', mustacheExpress());
