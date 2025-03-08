afd4cd9c9cc70bb95ec4dbefff54e9166ca2c122
Gaurav Manchanda <gaurav.manchanda@nokia.com>
2025-03-08 15:23:08 -0500

    Added route to insert new tasks

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -10,4 +10,12 @@ router.get('/', (req, res) => {
     });
 });
 
+router.post('/add', (req, res) => {
+    const { title, description, due_date, priority } = req.body;
+    TaskModel.addTask(title, description, due_date, priority, (err) => {
+        if (err) throw err;
+        res.redirect('/tasks');
+    });
+});
+
 module.exports = router;
