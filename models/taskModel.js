a472090b4dedb67fa7bb26680b4b7cd549c5f492
Gaurav Manchanda <gaurav.manchanda@nokia.com>
2025-03-08 15:19:20 -0500

    Fixed the position of closing }

--- models/taskModel.js
+++ models/taskModel.js
@@ -4,12 +4,11 @@ class TaskModel {
     static getAllTasks(callback) {
         db.all("SELECT * FROM tasks", [], callback);
     }
-}
 
     static addTask(title, description, due_date, priority, callback) {
         db.run("INSERT INTO tasks (title, description, due_date, priority, status) VALUES (?, ?, ?, ?, 'Pending')",
             [title, description, due_date, priority], callback);
-    }   
-
+    }
+}
 
 module.exports = TaskModel;
